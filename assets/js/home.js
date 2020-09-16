e34372fd0dbfd469b06e1a8cdbe24b3a343e41c9
OdrinskayaMargarita <margarita.odrinskaya@gmail.com>
2020-09-16 12:46:27 +0300

    some new pages

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     slidesToScroll: 1,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 1000,
     dots: true,
     customPaging : function(slider, i) {
       var thumb = $(slider.$slides[i]).data();
@@ -58,32 +58,15 @@ $(document).ready(function(){
     $(this).addClass('active');
   });
 
-
-  var height1 = $(window).height(); 
-  var width1 = $(window).width();
-    if(height1 >= '899' && width1>='1399') {
-      $('#fullpage').fullpage({
-        anchors : [ 'Home','About_Us','Plans_And_Priciing','Affilates','News','FAQ-anch', 'Contact_Us',],
-        navigation: true,
-        menu:'#navigation',
-        scrollOverflow:true,
-        scrollingSpeed : 1200 ,
-        afterLoad : function(origin, destination, direction){
-          var loadedSection = this;
-          if(origin.anchor == 'Plans_And_Priciing'){
-            $('.pr1').addClass('pr70');
-            $('.pr2').addClass('pr30');
-            $('.pr3').addClass('pr20');
-          }
-        },
-        afterSlideLoad: function( section, origin, destination, direction){
-          var loadedSlide = this;
-          if(section.anchor == 'Plans_And_Priciing'){
-            alert("First slide loaded");
-          }
-        }
-      });
-    };      
+  var customSelect = $('.name-money');
+  jcf.setOptions('Select', {
+    wrapNative: false,
+    wrapNativeOnMobile: false,
+    fakeDropInBody: false,
+    maxVisibleItems: 5
+  });
+  
+  jcf.replace(customSelect);
 
     $(window).scroll(function() {
 
@@ -122,7 +105,7 @@ $(document).ready(function(){
         var w_height = $(window).height(); // Высота окна браузера
         var d_height = $(document).height(); // Высота всего документа
         var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-        if (w_top +800 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+        if (w_top +700 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
             $('.count-spin').css('opacity', '1');
             $('.count-spin').spincrement({
                 thousandSeparator: " ",
---
+++ assets/js/main-personal.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $('body').css('opacity','1')
+
+  $( ".showText" ).click(function(event) {
+    event.preventDefault();
+    $('.all-hidd').stop().slideToggle();
+  });
+
+  $( ".hiddenText" ).click(function(event) {
+    var x = $(this).text();
+    var y = $(".showText").text();
+    $(this).text(y);
+    $(".showText").text(x);
+    $('.all-hidd').stop().slideToggle();
+  });
+
+  $( "main" ).click(function(event) {
+    $('.all-hidd').slideUp();
+  });
+ 
+  $( ".burger" ).click(function(event) {
+    $('.burg-menu').stop().toggleClass('open-nav-personal');
+  });
+ 
+  $( ".mob-nav-ul > li > a" ).click(function(event) {
+    $('.burg-menu').stop().removeClass('open-nav');
+  });
+ 
+});
+
